8f729cf5bbc38a8e558f498848533159d41988f1
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-23 10:12:38 +0700

    upload: api/_base.js

---
+++ api/_base.js
@@ -0,0 +1,14 @@
+const BASE = 'https://nanzz-musify.netlify.app';
+
+function cors(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return true;
+  }
+  return false;
+}
+
+module.exports = { BASE, cors };
